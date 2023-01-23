274c6de4a81622a0d5fe6ae636940ac05c31aedd
IMNOTXS <xs90035190@gmail.com>
2023-01-23 22:13:41 +0300

    added hero icons

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,6 +1,12 @@
 import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
+import {
+  UserIcon,
+  SearchIcon,
+  AdjustmentsIcon,
+  ChevronDownIcon,
+}from "react-native-heroicons/outline"
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -12,7 +18,7 @@ const HomeScreen = () => {
   },[])
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className='bg-white pt-5'>
       <Text className="text-red-500">
         <View className="flex-row pb-3 items-center mx-4 space-x-2">
           <Image
@@ -22,12 +28,13 @@ const HomeScreen = () => {
             className="h-7 w-7 bg-gray-300 p-4 rounded-full"
           />
 
-          <View>
+          <View className='flex-1'>
             <Text className="font-bold text-gray-400 text-xs">
               Deliver Now!</Text>
             <Text className="font-bold text-xl">
-              Current location</Text>
+              Current location <ChevronDownIcon size={20} color="#00CCBB"/> </Text>
           </View>
+          <UserIcon size={35} color="#00CCBB" />
         </View>
       </Text>
     </SafeAreaView>
