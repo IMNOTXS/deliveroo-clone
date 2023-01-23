3a31a80bfd05a3b5f80a2a52785ece9bb281b1d3
IMNOTXS <xs90035190@gmail.com>
2023-01-23 21:56:39 +0300

    styled the texts

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, Image } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
@@ -13,7 +13,23 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView>
-      <Text className="text-red-500">HomeScreen</Text>
+      <Text className="text-red-500">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+          <Image
+            source={{
+              uri: 'https://links.papareact.com/wru'
+            }} 
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+          />
+
+          <View>
+            <Text className="font-bold text-gray-400 text-xs">
+              Deliver Now!</Text>
+            <Text className="font-bold text-xl">
+              Current location</Text>
+          </View>
+        </View>
+      </Text>
     </SafeAreaView>
   )
 }
