3529e3db299fb8ec1ba1d88bfab6522fd4f2982f
IMNOTXS <xs90035190@gmail.com>
2023-01-24 16:15:34 +0300

    fixed errors

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,13 @@
-import { View, Text, SafeAreaView, Image } from 'react-native'
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import {
   UserIcon,
-  SearchIcon,
-  AdjustmentsIcon,
+  MagnifyingGlassIcon,
   ChevronDownIcon,
-}from "react-native-heroicons/outline"
+  AdjustmentsHorizontalIcon,
+} from "react-native-heroicons/outline"
+import Categories from '../components/Categories'
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -17,26 +18,38 @@ const HomeScreen = () => {
     });
   },[])
 
+ 
   return (
     <SafeAreaView className='bg-white pt-5'>
-      <Text className="text-red-500">
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
-          <Image
-            source={{
-              uri: 'https://links.papareact.com/wru'
-            }} 
-            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
-          />
+          <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
+            <Image source={{uri: 'https://links.papareact.com/wru'}} className="h-7 w-7 bg-gray-300 p-4 rounded-full"/>
 
-          <View className='flex-1'>
-            <Text className="font-bold text-gray-400 text-xs">
-              Deliver Now!</Text>
-            <Text className="font-bold text-xl">
-              Current location <ChevronDownIcon size={20} color="#00CCBB"/> </Text>
+            <View className='flex-1'>
+              <Text className="font-bold text-gray-400 text-xs">Deliver Now!</Text>
+              <Text className="font-bold text-xl">Current location <ChevronDownIcon size={20} color="#00CCBB"/> </Text>
+            </View>
+
+            <UserIcon size={35} color="#00CCBB" />
           </View>
-          <UserIcon size={35} color="#00CCBB" />
-        </View>
-      </Text>
+
+        <View className='flex-row items-center space-x-2 pb-2 mx-4 px-4'> 
+          <View className='flex-row flex-1 space-x-2 bg-gray-200 p-3'>
+            <MagnifyingGlassIcon color='gray' size={20} />
+
+            <TextInput placeholder='Resturaunts and Cuisines' keyboardType='default'/>
+          </View>
+
+          <AdjustmentsHorizontalIcon color='#00CCBB' />
+        </View> 
+        <ScrollView
+        className='bg-gray-100'
+        contentContainerStyle={{
+          paddingBottom: 100,
+        }}>
+
+          <Categories />
+
+        </ScrollView>
     </SafeAreaView>
   )
 }
