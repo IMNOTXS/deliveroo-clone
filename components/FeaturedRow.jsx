f71d3637bff3e8d57776aa8fce7229dd2d783b0c
IMNOTXS <xs90035190@gmail.com>
2023-01-24 22:08:37 +0300

    fixed the tailwind css error

--- components/FeaturedRow.jsx
+++ components/FeaturedRow.jsx
@@ -1,15 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { ArrowRightIcon } from 'react-native-heroicons/outline'
-import { fontScale } from 'nativewind'
+
 
 const FeaturedRow = ({title, description, id }) => {
   return (
     <View>
-      <View>
-        <Text style={{fontWeight: 'bold',fontSize: 18}}>{title}</Text>
-        <ArrowRightIcon />
+      <View className="mt-4 flex-row items-center justify-between px-4" >
+        <Text className="font-bold text-lg" >{title}</Text>
+        <ArrowRightIcon color='#00CCBB' />
       </View>
+      <Text className="text-xs text-gray-500 px-4">{description}</Text>
     </View>
   )
 }
